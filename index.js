75248b9d20f3596982f148f0e9dc60105fcb39f3
Turobjon <avezovzamir337@gmail.com>
2024-06-12 17:46:38 +0500

    fgh

---
+++ index.js
@@ -0,0 +1,29 @@
+let cards =  document.getElementById('cards')
+let url =  'https://jsonplaceholder.typicode.com/users'
+
+fetch(url)
+  .then((response) => response.json() ) 
+  .then((res) => getData((res)) ) 
+  .catch((err) => console.log(err) )
+
+
+  function getData(data) {
+    for (const iterator of data) {
+        let card = document.createElement('div')
+        let btn = document.createElement('button')
+        let p = document.createElement('p')
+        let h3 = document.createElement('h3')
+
+
+        btn.innerHTML = 'More ' + iterator?.id;
+        h3.innerHTML = 'More ' + iterator?.title;
+        p.innerHTML = 'More ' + iterator?.body;
+        card.classList.add('card')
+        card.appendChild(h3)
+        card.appendChild(p)
+        card.appendChild(btn)
+        cards.appendChild(card)
+
+    }
+    
+  }
